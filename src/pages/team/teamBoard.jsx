3161d018ca79d0bafa5a54148ee6a1ea210e42a1
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-11-21 01:51:34 +0530

    Update teamBoard.jsx

--- src/pages/team/teamBoard.jsx
+++ src/pages/team/teamBoard.jsx
@@ -5,25 +5,39 @@ import TeamAvatar from "./teamAvatar";
 
 
 const Container = styled(Box)`
-  height: 250px;
-  width: 90vw;
-  margin-bottom: 40px;
+  width: 95vw;
+  margin: 20px 0px;
   position: relative;
   align-items: center;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   cursor: pointer;
   border-radius: 7px;
-  ${mobile({width: "400px", height: "200px" })}
+  box-shadow: 0px 0px 2px 2px #bdbdbd;
+  ${mobile({width: "95vw" })}
 `;
 
-const Heading = styled('h2')`
-    font-weight: 500;
+const Info = styled(Box)`
+    width: 25vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `
+
+const Heading = styled('h4')`
+    font-weight: 500;
+    letter-spacing: 1px;
+    text-align: center;
+`
+
+const Typography = styled('p')`
+    font-size: 16px;
+    text-align: center;
+`
+
 const Wrapper = styled(Box)`
-  width: 100%;
-  height: 100%;
+  width: 70vw;
   position: relative;
   margin-bottom: 1px;
   align-items: center;
@@ -32,8 +46,7 @@ const Wrapper = styled(Box)`
   justify-content: center;
   cursor: pointer;
   border-radius: 7px;
-  border: 1px solid gray ;
-  ${mobile({width: "400px", height: "200px" })}
+  ${mobile({width: "70vw", height: "200px" })}
 `;
 
 const TeamBoard = ({ item, index }) => {
@@ -49,13 +62,17 @@ const TeamBoard = ({ item, index }) => {
         item ?
         (
           <Container onClick={() => handleClick(item)}>
-            <Heading> {`TEAM - ${index+1}`} </Heading>
+            <Info>
+                <Heading> {`Team No. - ${index+1}`}</Heading>
+                <Typography> {`Total Members - ${item?.team_members.length}`}</Typography>
+            </Info>
+            
             <Wrapper onClick={handleClick}>
             {
-                <Grid container spacing={1}>  
+                <Grid container spacing={0}>  
                 {
                     item?.team_members.map((item, index) => (
-                    <Grid item xs={3} sm={3} md={3} lg={2} xl={1} key={item._id}> 
+                    <Grid item xs={4} sm={3} md={2} lg={2} xl={1} key={item._id}> 
                         <TeamAvatar key={item._id} item={item} index={index}/>  
                     </Grid>               
                 ))
