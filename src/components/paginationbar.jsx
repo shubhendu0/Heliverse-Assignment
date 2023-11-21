9d1a4510e44c163adc844d19e431fb22f7eab683
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-11-21 14:37:49 +0530

    Update paginationbar.jsx

--- src/components/paginationbar.jsx
+++ src/components/paginationbar.jsx
@@ -3,14 +3,13 @@ import { Box, Pagination, PaginationItem} from '@mui/material';
 import { ArrowForward, ArrowBack} from '@mui/icons-material';
 import { useSelector } from 'react-redux';
 
-
 const PaginationBar = ({currentPage, setCurrentPage}) => {
     const totalPages = useSelector(state => state.user.totalPages)
 
     return (
         <Box
         width="100vw"
-        margin="40px"
+        margin="40px 0px"
         display="flex"
         justifyContent="center"
         >
